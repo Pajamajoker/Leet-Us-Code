1ed8850a1ea1538ae52f0af7a71cd4aa48047cfb
Pajamajoker <46530662+Pajamajoker@users.noreply.github.com>
2022-08-12 15:15:31 +0530

    day-11

---
+++ Day-11/Kth Element Of Two Sorted Arrays.cpp
@@ -0,0 +1,43 @@
+class Solution{
+    public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        int p1 = 0, p2 = 0;
+        int cntr = 0;
+        while(p1 < n && p2 < m)
+        {
+            if(arr1[p1] < arr2[p2])
+            {
+                cntr++;
+                if(cntr == k)
+                {
+                    return arr1[p1];
+                }
+                p1++;
+            }
+            else 
+            {
+                cntr++;
+                if(cntr == k)
+                {
+                    return arr2[p2];
+                }
+                p2++;
+            }
+        }
+        while(p1 < n)
+        {
+            cntr++;
+            if(cntr == k)
+            return arr1[p1];
+            p1++;
+        }
+        while(p2 < m)
+        {
+            cntr++;
+            if(cntr == k)
+            return arr2[p2];
+            p2++;
+        }
+    }
+};
