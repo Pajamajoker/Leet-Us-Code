0473fe83806bc07017dc527910cd8589b557e421
Pajamajoker <46530662+Pajamajoker@users.noreply.github.com>
2022-08-14 15:19:27 +0530

    day-13 Stacks & Queues - 1

---
+++ Day-13/Balanced Parenthesis.cpp
@@ -0,0 +1,13 @@
+bool isValid(string s) {
+        if(s.size() % 2 != 0) return false;
+        stack<char> stk;
+        for(auto it: s){
+            if(it == '(' || it == '{' || it == '[') stk.push(it);
+            else if(it == ')' && !stk.empty() && stk.top() == '(') stk.pop();
+            else if(it == '}' && !stk.empty() && stk.top() == '{') stk.pop();
+            else if(it == ']' && !stk.empty() && stk.top() == '[') stk.pop();
+            else return false;
+        }
+        
+        return stk.empty();
+    }
